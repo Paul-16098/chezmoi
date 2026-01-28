5b21ed7ee41b6b096964c4ab9c09a3acf1faa28a
Paul-16098 <pl816098@gmail.com>
2026-01-29 07:59:47 +0800

    Update AppData/Roaming/yazi/config/init.lua

--- AppData/Roaming/yazi/config/init.lua
+++ AppData/Roaming/yazi/config/init.lua
@@ -124,16 +124,12 @@ end
 require("bunny"):setup({
     hops = {
         { key = "/",          path = "/", },
-        { key = "t",          path = "/tmp", },
-        { key = "n",          path = "/nix/store",     desc = "Nix store" },
         { key = "~",          path = "~",              desc = "Home" },
         { key = "m",          path = "~/Music",        desc = "Music" },
-        { key = "d",          path = "~/Desktop",      desc = "Desktop" },
         { key = "D",          path = "~/Documents",    desc = "Documents" },
         { key = "c",          path = "~/.config",      desc = "Config files" },
         { key = { "l", "s" }, path = "~/.local/share", desc = "Local share" },
         { key = { "l", "b" }, path = "~/.local/bin",   desc = "Local bin" },
-        { key = { "l", "t" }, path = "~/.local/state", desc = "Local state" },
         -- key and path attributes are required, desc is optional
     },
     desc_strategy = "path", -- If desc isn't present, use "path" or "filename", default is "path"
