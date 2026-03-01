dbd3281e640a7f77269174ddce2374a16fb1c650
Paul-16098 <pl816098@gmail.com>
2026-03-01 10:51:39 +0800

    Update "readonly_OneDrive/\346\226\207\344\273\266/PowerShell/Microsoft.PowerShell_profile.ps1"

--- readonly_OneDrive/readonly_文件/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_OneDrive/readonly_文件/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -9,3 +9,13 @@ Import-Module -Name Microsoft.WinGet.CommandNotFound
 Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 carapace _carapace | Out-String | Invoke-Expression
+
+atuin init powershell --disable-up-arrow | Out-String | Invoke-Expression
+
+# 将 Ctrl+/ 绑定到 Ctrl+r 的功能（atuin search）
+Set-PSReadLineKeyHandler -Chord "Ctrl+/" -BriefDescription "Runs Atuin search" -ScriptBlock {
+    $atuinModule = Get-Module Atuin
+    if ($atuinModule) {
+        & $atuinModule { Invoke-AtuinSearch }
+    }
+}
