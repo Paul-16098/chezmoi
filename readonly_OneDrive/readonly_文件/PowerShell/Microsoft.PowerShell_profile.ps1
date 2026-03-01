c5f25c3be10beb0109b3c24ff4e47a432f850ae0
Paul-16098 <pl816098@gmail.com>
2026-03-01 10:27:30 +0800

    Update "readonly_OneDrive/\346\226\207\344\273\266/PowerShell/Microsoft.PowerShell_profile.ps1"

--- readonly_OneDrive/readonly_文件/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_OneDrive/readonly_文件/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -5,3 +5,7 @@ Invoke-Expression (&starship init powershell)
 
 Import-Module -Name Microsoft.WinGet.CommandNotFound
 #f45873b3-b655-43a6-b217-97c00aa0db58
+
+Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+carapace _carapace | Out-String | Invoke-Expression
